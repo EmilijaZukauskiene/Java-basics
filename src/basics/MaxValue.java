d901f140030ac3aa890fa3e69864b5a3ad20f76f
EmilijaZukauskiene <emilija.ramanauskaite@gmail.com>
2019-12-07 14:15:34 +0200

    Max Value

---
+++ src/basics/MaxValue.java
@@ -0,0 +1,11 @@
+package basics;
+
+public class MaxValue {
+    public static void main(String[] args) {
+        int num1 =Integer.MAX_VALUE;
+        int num2 = Integer.MAX_VALUE;
+        long num3 = (long) num1 + num2;
+
+        System.out.println(num3);
+    }
+}
