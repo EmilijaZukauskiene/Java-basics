e5387794fcb6cf083fbb809ade56acbec07d13c2
EmilijaZukauskiene <emilija.ramanauskaite@gmail.com>
2019-12-07 14:02:59 +0200

    Third exercise

---
+++ src/basics/ThirdExercise.java
@@ -0,0 +1,13 @@
+package basics;
+
+
+public class ThirdExercise {
+    public static void main(String[] args) {
+
+        int a = 5;
+        int b = 7;
+        double skaicius = (double) a / b;
+
+        System.out.printf("%.3f", skaicius);
+    }
+}
